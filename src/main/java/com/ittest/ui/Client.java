ac753284e8fbe0e13cbb741370b6d966bbcff268
anliping <anliping@bitnei.cn>
2020-01-04 22:56:13 +0800

    spring

--- src/main/java/com/ittest/ui/Client.java
+++ src/main/java/com/ittest/ui/Client.java
@@ -1,11 +1,12 @@
 package com.ittest.ui;
 
+import com.ittest.factory.BeanFactory;
 import com.ittest.service.IAccountService;
-import com.ittest.service.impl.AccountServiceImpl;
 /*模拟一个表现层，用于调用业务层*/
 public class Client {
     public static void main(String[] args) {
-        IAccountService as=new AccountServiceImpl();
+       // IAccountService as=new AccountServiceImpl();
+        IAccountService as= (IAccountService) BeanFactory.getBean("accountService");
         as.saveAccount();
     }
 }
