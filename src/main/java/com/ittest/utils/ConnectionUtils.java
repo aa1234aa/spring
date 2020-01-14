e146a3af4b35636823ea01975c8b0bbbf96f1a26
John Doe <john@sample.com>
2020-01-14 10:33:42 +0800

    spring

--- src/main/java/com/ittest/utils/ConnectionUtils.java
+++ src/main/java/com/ittest/utils/ConnectionUtils.java
@@ -1,5 +1,31 @@
 package com.ittest.utils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /*连接的工具类，它用于从数据源中获取一个连接，实现和线程的绑定*/
 public class ConnectionUtils {
+   private ThreadLocal<Connection> tl=new ThreadLocal<Connection>();
 
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private DataSource dataSource;
+   /*获取当前线程上的连接*/
+    public Connection getThreadConnection(){
+        Connection conn=tl.get();
+        try {
+            if (conn==null){
+                conn=dataSource.getConnection();
+                tl.set(conn);
+            }
+            return conn;
+
+        } catch (Exception e) {
+            throw new
+                    RuntimeException(e);
+        }
+    }
 }
